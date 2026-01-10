8ee0cae97fb08704bff733defb1cc381219f185e
dmytro-chushko <dmytro.chushko@gmail.com>
2026-01-10 21:31:58 +0200

    Add results ui

--- src/components/dashboard/FormList.tsx
+++ src/components/dashboard/FormList.tsx
@@ -1,16 +1,27 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+
 import { useFormList } from '@/features/forms/lib/use-form-list';
+
+import { Button } from '../ui/button';
+import { Field } from '../ui/field';
 
 import { CreateForm } from './CreateForm';
 import { FormTable } from './FormTable';
 
 export function FormList() {
   const { formList } = useFormList();
+  const router = useRouter();
 
   return (
     <div>
-      <CreateForm />
+      <Field orientation="horizontal">
+        <CreateForm />
+        <Button type="button" onClick={() => router.push('dashboard/results')}>
+          Results
+        </Button>
+      </Field>
       {formList && <FormTable forms={formList} />}
     </div>
   );
